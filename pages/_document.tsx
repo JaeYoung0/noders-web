51f6da93b967d6dc6657002d69fe455902264470
JaeYoung0 <hjy8697@gmail.com>
2022-07-18 00:55:36 +0900

    fix: add Meta to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@
 // client side render styles ~ server side render styles 일치여부 체크
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { extractCritical } from '@emotion/server'
+import Meta from '@/components/Meta'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -25,6 +26,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <Meta />
           <link rel="icon" href="favicon.ico" />
           <link rel="preload" href="/fonts/SUIT-Bold.woff2" as="font" />
           <link rel="preload" href="/fonts/SUIT-Regular.woff2" as="font" />
--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -3,35 +3,38 @@ const CONSTANTS = {
   description: 'CONNECT IDEAS TO REALITY. 노더스입니다.',
   siteUrl: 'https://www.noders-studio.com/',
   imageUrl: 'https://www.noders-studio.com/images/img-meta.png',
-  keywords: 'CONNECT IDEAS TO REALITY. 노더스입니다.',
+  keywords: '브랜딩, 패키지, 로고, 웹디자인, 웹앱개발',
 }
-const Meta = () => (
-  <>
-    <meta name="description" content={CONSTANTS.description} />
-    <meta content={CONSTANTS.keywords} name="keywords" />
-    <meta name="robots" content="all" />
-    <meta content="ko" name="locale" />
-    <meta content="KRW" name="currency" />
-    <meta content="web" name="environment" />
 
-    {/* Meta - Open Graph */}
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={CONSTANTS.title} />
-    <meta property="og:description" content={CONSTANTS.description} />
-    <meta property="og:image" content={CONSTANTS.imageUrl} />
-    <meta property="og:url" content={CONSTANTS.siteUrl} />
-    <meta property="og:site_name" content="스쿨라이브" />
-    <meta property="og:locale" content="ko-KR" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
+function Meta() {
+  return (
+    <>
+      <meta name="description" content={CONSTANTS.description} />
+      <meta content={CONSTANTS.keywords} name="keywords" />
+      <meta name="robots" content="all" />
+      <meta content="ko" name="locale" />
+      <meta content="KRW" name="currency" />
+      <meta content="web" name="environment" />
 
-    {/* Meta - Twitter */}
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content={CONSTANTS.title} />
-    <meta name="twitter:description" content={CONSTANTS.description} />
-    <meta name="twitter:image" content={CONSTANTS.imageUrl} />
-    <meta name="twitter:url" content={CONSTANTS.siteUrl} />
-  </>
-)
+      {/* Meta - Open Graph */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={CONSTANTS.title} />
+      <meta property="og:description" content={CONSTANTS.description} />
+      <meta property="og:image" content={CONSTANTS.imageUrl} />
+      <meta property="og:url" content={CONSTANTS.siteUrl} />
+      <meta property="og:site_name" content="스쿨라이브" />
+      <meta property="og:locale" content="ko-KR" />
+      <meta property="og:image:width" content="513" />
+      <meta property="og:image:height" content="540" />
+
+      {/* Meta - Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={CONSTANTS.title} />
+      <meta name="twitter:description" content={CONSTANTS.description} />
+      <meta name="twitter:image" content={CONSTANTS.imageUrl} />
+      <meta name="twitter:url" content={CONSTANTS.siteUrl} />
+    </>
+  )
+}
 
 export default Meta
